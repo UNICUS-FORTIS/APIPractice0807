689580d6193bc6fe7a8c7c6b6d2f0bc439efd4d7
Louie Shin <unicus.fortis@gmail.com>
2023-08-07 22:49:25 +0900

    Initial Commit

---
+++ APIPractice0807/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APIPractice0807
+//
+//  Created by LOUIE MAC on 2023/08/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
